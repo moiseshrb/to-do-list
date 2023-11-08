9f8d561cb13a72f3aee21985c94a9522f3add44c
MoisesBarbosa1 <henriquebrbx@gmail.com henriquebrbx@gmail.com>
2023-11-08 12:47:00 -0300

    ''

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require('express');
+const checklistRouter = require('./src/routes/checklist');
+const app = express();
 
+app.use(express.json());
+
+app.use(checklistRouter);
+
+app.listen(3000, () => {
+  console.log('Servidor foi iniciado');
+})
